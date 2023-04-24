b5c1707dd8d8c9b4f7c49d98cd93952f38c1d07c
janetakagi <127626133+janetakagi@users.noreply.github.com>
2023-04-24 12:52:03 -0600

    Update plots_w_corr.py

--- plots_w_corr.py
+++ plots_w_corr.py
@@ -4,9 +4,7 @@ import matplotlib.pyplot as plt
 
 def mk_plots():
     df = pd.read_csv("correlation_augmented_metadata.csv")
-    # print(df)
-    variables = ['meanPercentDC_Imperfectly','meanPercentDC_ModeratelyWell','meanPercentDC_Poor','meanPercentDC_SomewhatExcessive','meanPercentDC_VeryPoor','meanPercentDC_Well'] 
-    # variables = ['strmOrder','Magnitude','strmDrop','WSNO','length_km','area_sqkm','drain_den','gelev_m','garea_sqkm','gord','PathLength','TotalLength','cls1','cls10','cls11','cls12','cls2','cls3','cls4','cls5','cls6','cls7','cls8','cls9','Dam_SurfaceArea','Dam_Count','HydroLakes_Area_sqkm','MeanPopden_2000','MeanPopden_2005','MeanPopden_2010','MeanPopden_2015','MeanHumanFootprint','meanPercentDC_Imperfectly','meanPercentDC_ModeratelyWell','meanPercentDC_Poor','meanPercentDC_SomewhatExcessive','meanPercentDC_VeryPoor','meanPercentDC_Well']
+    variables = ['strmOrder','Magnitude','strmDrop','WSNO','length_km','area_sqkm','drain_den','gelev_m','garea_sqkm','gord','PathLength','TotalLength','cls1','cls10','cls11','cls12','cls2','cls3','cls4','cls5','cls6','cls7','cls8','cls9','Dam_SurfaceArea','Dam_Count','HydroLakes_Area_sqkm','MeanPopden_2000','MeanPopden_2005','MeanPopden_2010','MeanPopden_2015','MeanHumanFootprint','meanPercentDC_Imperfectly','meanPercentDC_ModeratelyWell','meanPercentDC_Poor','meanPercentDC_SomewhatExcessive','meanPercentDC_VeryPoor','meanPercentDC_Well']
     for var in variables:
         plt.scatter(df[var],df["correlation_q_et"],label='evaporation')
         plt.scatter(df[var],df["correlation_q_t"],label='temperature')
